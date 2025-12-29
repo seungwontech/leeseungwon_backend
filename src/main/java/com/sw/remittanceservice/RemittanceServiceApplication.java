e2e97e4fcce7e1520339a883b77ee992ce4e463f
seungwonlee <swlee173@gmail.com>
2025-12-29 16:38:25 +0900

    init

---
+++ src/main/java/com/sw/remittanceservice/RemittanceServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sw.remittanceservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemittanceServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemittanceServiceApplication.class, args);
+    }
+
+}
