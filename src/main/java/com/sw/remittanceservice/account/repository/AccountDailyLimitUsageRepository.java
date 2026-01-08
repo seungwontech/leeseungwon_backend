770165043c501109ba41014f00790b6545bf42a4
seungwonlee <swlee173@gmail.com>
2026-01-08 19:28:02 +0900

    fix: 일일 송금 한도 업데이트 시 동시성 제어 추가

--- src/main/java/com/sw/remittanceservice/account/repository/AccountDailyLimitUsageRepository.java
+++ src/main/java/com/sw/remittanceservice/account/repository/AccountDailyLimitUsageRepository.java
@@ -1,7 +1,9 @@
 package com.sw.remittanceservice.account.repository;
 
 import com.sw.remittanceservice.account.entity.AccountDailyLimitUsage;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,10 +12,8 @@ import java.time.LocalDate;
 import java.util.Optional;
 
 public interface AccountDailyLimitUsageRepository extends JpaRepository<AccountDailyLimitUsage, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<AccountDailyLimitUsage> findByAccountIdAndLimitDate(Long accountId, LocalDate limitDate);
-
-
-
 
     @Query(
             value = "update account_daily_limit_usage set withdraw_used = :withdrawUsed " +
